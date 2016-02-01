96743d12d87f050b3a2af445d89adf9390ecfd39
loligans <lindemann.ethan@gmail.com>
2016-02-01 14:53:17 -0800

    Benchmarked problem 7. 10001st Prime Number. Best time I got was 0.006594 Seconds

--- 7. 10001st Prime/Main.c
+++ 7. 10001st Prime/Main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #define SIEVE_SIZE 250000
 #define PRIME_INDEX 10001
 #define NOT_PRIME 0
@@ -9,16 +10,29 @@ long*generateSieve();
 
 int main(int argc, char* argv[])
 {
+	clock_t begin, end;
+	double time_spent;
 	printf("Locating the 10,001st prime number\n");
+	printf("Generating Sieve of size %d\n", SIEVE_SIZE);
+
+	begin = clock();
 	long* sieve = generateSieve();
+	end = clock();
+	double time_Spent_Generating = (double) (end - begin) / CLOCKS_PER_SEC;
+	begin = clock();
 	long nthPrime = findNthPrime(PRIME_INDEX, sieve);
+	end = clock();
+	double time_Spent_Locating = (double) (end - begin) / CLOCKS_PER_SEC;
+	
+	printf("It took %f seconds to generate the sieve\n", time_Spent_Generating);
+	printf("It took %f seconds to locate the 10,001st prime\n", time_Spent_Locating);
+	printf("The total CPU time is %f\n", time_Spent_Generating + time_Spent_Locating);
 	printf("The 10,001st prime number is %lu\n", nthPrime);
 	return nthPrime;
 }
 
 long* generateSieve()
 {
-	printf("Generating Sieve of size %d\n", SIEVE_SIZE);
 	long* sieve = malloc(sizeof(long) * SIEVE_SIZE);
 	//To make things less complicated we start the sieve at 2 (The first prime)
 	for(long a=2;a<=SIEVE_SIZE;++a)
---
+++ 9. Special Pythagorean Triplet/Main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define bool int
+#define FALSE 0
+#define TRUE !FALSE
+
+bool testIfCoprime(long m, long n);
+
+int main(int argc, char*argv[])
+{
+	
+	return 1;
+}
+
+bool testIfCoprime(long m, long n)
+{
+	
+	return TRUE;
+}
