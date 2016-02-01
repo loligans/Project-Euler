3347d5141838d472fb3a05a98f980fd88d4780f4
loligans <lindemann.ethan@gmail.com>
2016-02-01 01:41:58 -0800

    finished problem 7. 10001st Prime using the Eratosthenes method

--- 7. 10001st Prime/Main.c
+++ 7. 10001st Prime/Main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIEVE_SIZE 120
-#define bool int
-#define FALSE 0
-#define TRUE !FALSE
+#define SIEVE_SIZE 250000
+#define PRIME_INDEX 10001
+#define NOT_PRIME 0
 
 long findNthPrime(int, long*);
 long*generateSieve();
@@ -12,92 +11,55 @@ int main(int argc, char* argv[])
 {
 	printf("Locating the 10,001st prime number\n");
 	long* sieve = generateSieve();
-	findNthPrime(100001, sieve);
-	printf("\n");
-	return 1;
+	long nthPrime = findNthPrime(PRIME_INDEX, sieve);
+	printf("The 10,001st prime number is %lu\n", nthPrime);
+	return nthPrime;
 }
 
 long* generateSieve()
 {
 	printf("Generating Sieve of size %d\n", SIEVE_SIZE);
 	long* sieve = malloc(sizeof(long) * SIEVE_SIZE);
-	for(int a=0;a<SIEVE_SIZE - 1;++a)
-		sieve[a] = a+2;
+	//To make things less complicated we start the sieve at 2 (The first prime)
+	for(long a=2;a<=SIEVE_SIZE;++a)
+		sieve[a] = a;
 	return sieve;
 }
 
 //Using the Sieve of Eratosthenes method
 long findNthPrime(int primeIndexToFind, long* sieve)
 {
+	//Allocate an array up to the size of the highest prime we want
+	long* primesArray = malloc(sizeof(long) * PRIME_INDEX);
+	long primesFound = 0;
 
-	long primeFound = -1;
-	for(int a=0;a<SIEVE_SIZE;++a)
+	//Loop through the Sieve and find all non-primes 
+	for(long a=2;a<SIEVE_SIZE;++a)
 	{
-		if(sieve[a] == 0)
+		//Skip this index if its value is NOT_PRIME
+		if(sieve[a] == NOT_PRIME)
 			continue;
-	}
+		else 
+		//We have come up to the next non-zero value to test
+		{
+			//Remove all multiples frome the current value of a
+			for(long b=a+a; b<SIEVE_SIZE;b+=a)
+			{
+				if(sieve[b] == NOT_PRIME)
+					continue;
+				else if(sieve[b] % b == 0)
+					sieve[b] = NOT_PRIME;
+			}//End For
+			//Save the prime in our primes array
+			primesArray[primesFound] = a;
+			primesFound++;
+		}//End Else
 
-
-	return primeFound;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-	
-	// long* primesFound = malloc(sizeof(long) * primeToFind);
-	// while(currentPrimeIndex < primeToFind)
-	// {
-	// 	if (value % nextPrimeToCheck == 0)
-	// 	{
-	// 		//Check if we found a new Max Prime
-	// 		largestPrimeFound = largestPrimeFound < nextPrimeToCheck ? nextPrimeToCheck : largestPrimeFound;
-	// 		//Set the new value of the Prime we are checking
-	// 		value /= nextPrimeToCheck;
-	// 		totalCalculation *= nextPrimeToCheck;
-	// 		//Check if we have found the largest prime
-	// 		printf("MPP=%lg : nextPrimeToCheck=%lu : largestPrimeFound=%lu : totalCalculation=%lu\n",
-	// 				MPP, 	  nextPrimeToCheck, 	 largestPrimeFound,		 totalCalculation);
-			
-	// 		if (value == 1)
-	// 			return largestPrimeFound;
-			
-	// 		//Adds the next value into nextPrimeToCheck
-	// 		nextPrimeToCheck = 2;
-	// 	}//End If
-	// 	else
-	// 		nextPrimeToCheck += nextPrimeToCheck % 2 ? 2 : 1;
-	// }//End While
+		//Make sure we do not access invalid memory,
+		//by storing another prime beyond the index we want
+		if(primeIndexToFind == primesFound)
+			break;
+	}//End For
+	//Arrays start at 0 so we subtract the offset to get out value
+	return primesArray[PRIME_INDEX - 1];
 }
